906f83d02ded3eb8d4d59e0024f43cec92f55dbe
JustinMa <jma162@my.bcit.ca>
2022-11-14 15:36:30 -0800

    path

---
+++ layout/index.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export const Layout = (props) => {
+  <div>
+    <Link href={`/register`}></Link>
+    <Link href={`/login`}></Link>
+    <Link href={`/posts`}></Link>
+    <Link href={`/profile`}></Link>
+  </div>;
+};
