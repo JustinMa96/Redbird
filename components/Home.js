e8f96957fb7b2d59b68a24b86578ba24da6c9359
Heidingg <tnguyen480@my.bcit.ca>
2022-12-06 14:19:17 -0800

    Creat a post page function not working yet

---
+++ components/Home.js
@@ -0,0 +1,63 @@
+
+import ShareAPost from "./ShareAPost";
+import Posts from "./Posts";
+import React, { useEffect, useRef, useState } from "react";
+import CreatePost from "../pages/CreatePost";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../firebase/firebaseConfig.js";
+import "../pages/createpost.js"
+import styled from "styled-components";
+import { useRouter } from "next/router";
+export default function Home() {
+
+  const [ loading, setLoading ] = useState(false);
+  const currentUser = useAuth();
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  async function handleLogout() {
+    setLoading(true);
+    try {
+      await logout();
+      window.location = "/";
+    } catch {
+      alert("Error!");
+    }
+    setLoading(false);
+  }
+
+  const [postList, setPostList] = useState([]);
+  const postsCollectionRef = collection(db, "posts");
+
+
+  useEffect(() => {
+    const getPosts = async () => {
+    const data = await getDocs(postsCollectionRef);
+    setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  };
+
+    getPosts();
+  });
+
+  return (
+    <div className="HomePage">
+        {postList.map((post) => {
+          return (
+              <div className="post">
+              <div className="postHeader">
+              <div className="title">
+                <h1>{post.title}</h1>
+                </div>
+                </div>
+                <div className="postTextContainer"> {post.postList}</div>
+                <h3>@{post.author.name}</h3>
+                </div>
+
+          );
+        })}
+      </div>
+  );
+}
+
+
